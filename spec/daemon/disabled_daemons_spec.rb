c4e38bcea572e16a8ca5627f3ce301d2dd242623
mgi166 <skskoari@gmail.com>
2014-02-16 09:15:07 +0900

    * Add new test associates CentOS daemons    * Check unnecessary daemons is whether not running

---
+++ spec/daemon/disabled_daemons_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require 'yaml'
+
+attributes  = YAML.load_file('./spec/server_attributes.yml')
+target_host = ENV['TARGET_HOST']
+
+(attributes[target_host][:disabled_daemons] || []).each do |service|
+  describe service(service) do
+    it { should_not be_enabled }
+    it { should_not be_running }
+  end
+end
