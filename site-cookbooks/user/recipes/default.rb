219b6d19ac9cfcc844b4d8222cc50c367c111bc0
mgi166 <skskoari@gmail.com>
2014-07-30 09:29:07 +0900

    * Add `user` recipe

--- site-cookbooks/user/recipes/default.rb
+++ site-cookbooks/user/recipes/default.rb
@@ -6,3 +6,25 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+name = 'mgi'
+
+user name do
+  action :create
+  password "$1$ijQNbBTK$//GirAKqmfH7I.qtlGro9."
+end
+
+directory "/home/#{name}/.ssh" do
+  action :create
+  owner name
+  group name
+  mode '700'
+end
+
+cookbook_file "/home/#{name}/.ssh/authorized_keys" do
+  action :create
+  owner name
+  group name
+  mode '600'
+  source 'id_rsa.pub'
+end
