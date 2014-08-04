350521e73d991297e12777527084e8c39d9a78ef
mgi166 <skskoari@gmail.com>
2014-08-04 09:56:00 +0900

    * user recipe forward in runlist * npm recipe comment out

--- site-cookbooks/npm/recipes/default.rb
+++ site-cookbooks/npm/recipes/default.rb
@@ -7,6 +7,20 @@
 # All rights reserved - Do Not Redistribute
 #
 
-npm_package "hubot" do
-  action :install
-end
+# npm_package "hubot" do
+#   action :install
+# end
+
+# npm_package "coffee-script" do
+#   action :install
+# end
+
+# execute "install hubot" do
+#   action :run
+#   command "npm install -g hubot"
+# end
+
+# execute "install coffee-script" do
+#   action :run
+#   command "npm install -g coffee-script"
+# end
