e6b7a3af5ee7a3d557c04d770b91303d187ff51b
mgi166 <skskoari@gmail.com>
2014-08-05 09:57:59 +0900

    * put bitbucket.id_rsa, .ssh/config

--- site-cookbooks/ssh_config/recipes/default.rb
+++ site-cookbooks/ssh_config/recipes/default.rb
@@ -6,3 +6,39 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+name = 'mgi'
+
+directory "/home/#{name}/.ssh/bitbucket" do
+  action :create
+  owner  name
+  group  name
+end
+
+directory "/home/#{name}/.ssh/target-host" do
+  action :create
+  owner  name
+  group  name
+end
+
+cookbook_file "/home/#{name}/.ssh/bitbucket/id_rsa" do
+  action :create
+  owner  name
+  group  name
+  mode  '600'
+  source 'bitbucket.id_rsa'
+end
+
+# cookbook_file "/home/#{name}/.ssh/target-host/id_rsa" do
+#   action :create
+#   owner  name
+#   mode  '600'
+#   group  name
+# end
+
+cookbook_file "/home/#{name}/.ssh/config" do
+  action :create
+  owner  name
+  group  name
+  source 'config'
+end
