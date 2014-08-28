2bb2a653d0b3f5d7230d28798bb8dc41ab6e40bd
mgi166 <skskoari@gmail.com>
2014-08-28 09:58:44 +0900

    * Create user `mgi` with `EncryptedDataBagItem`

--- site-cookbooks/user/recipes/default.rb
+++ site-cookbooks/user/recipes/default.rb
@@ -7,31 +7,43 @@
 # All rights reserved - Do Not Redistribute
 #
 
-name = 'mgi'
+# name = 'mgi'
 
-user name do
+# user name do
+#   action :create
+#   password "$1$ijQNbBTK$//GirAKqmfH7I.qtlGro9."
+# end
+
+# directory "/home/#{name}/.ssh" do
+#   action :create
+#   owner name
+#   group name
+#   mode '700'
+# end
+
+# cookbook_file "/home/#{name}/.ssh/authorized_keys" do
+#   action :create
+#   owner name
+#   group name
+#   mode '600'
+#   source 'authorized_keys'
+# end
+
+# cookbook_file "/etc/sudoers" do
+#   action :create
+#   owner 'root'
+#   group 'root'
+#   mode '440'
+# end
+
+data = Chef::EncryptedDataBagItem.load('user', 'mgi')
+
+group 'dev' do
   action :create
-  password "$1$ijQNbBTK$//GirAKqmfH7I.qtlGro9."
 end
 
-directory "/home/#{name}/.ssh" do
-  action :create
-  owner name
-  group name
-  mode '700'
-end
-
-cookbook_file "/home/#{name}/.ssh/authorized_keys" do
-  action :create
-  owner name
-  group name
-  mode '600'
-  source 'authorized_keys'
-end
-
-cookbook_file "/etc/sudoers" do
-  action :create
-  owner 'root'
-  group 'root'
-  mode '440'
+user data['id'] do
+  action   :create
+  group    data['group']
+  password `openssl passwd -1 #{data['password']}`.chomp
 end
