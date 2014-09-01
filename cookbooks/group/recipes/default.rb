cf2c6eb4dd2e15a796aa2c62a8cdf6df40ef0348
mgi166 <skskoari@gmail.com>
2014-09-01 09:17:49 +0900

    * knife cookbook create group -o cookbooks    * Creation of group is very popular

---
+++ cookbooks/group/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: group
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+group 'developer' do
+  action :create
+end
