df7e40389ac2d3dc8acf4066928f69fc8d3c261f
mgi166 <skskoari@gmail.com>
2014-09-01 09:24:44 +0900

    * Remove comment out and include_recipe 'group'

--- site-cookbooks/user/recipes/default.rb
+++ site-cookbooks/user/recipes/default.rb
@@ -7,43 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# name = 'mgi'
+include_recipe 'group'
 
-# user name do
-#   action :create
-#   password "$1$ijQNbBTK$//GirAKqmfH7I.qtlGro9."
+# data = Chef::EncryptedDataBagItem.load('user', 'mgi')
+
+# user data['id'] do
+#   action   :create
+#   group    data['group']
+#   password `openssl passwd -1 #{data['password']}`.chomp
 # end
-
-# directory "/home/#{name}/.ssh" do
-#   action :create
-#   owner name
-#   group name
-#   mode '700'
-# end
-
-# cookbook_file "/home/#{name}/.ssh/authorized_keys" do
-#   action :create
-#   owner name
-#   group name
-#   mode '600'
-#   source 'authorized_keys'
-# end
-
-# cookbook_file "/etc/sudoers" do
-#   action :create
-#   owner 'root'
-#   group 'root'
-#   mode '440'
-# end
-
-data = Chef::EncryptedDataBagItem.load('user', 'mgi')
-
-group 'dev' do
-  action :create
-end
-
-user data['id'] do
-  action   :create
-  group    data['group']
-  password `openssl passwd -1 #{data['password']}`.chomp
-end
