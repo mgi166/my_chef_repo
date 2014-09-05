1f79873b2f421337343b2be568db236effb0f633
mgi166 <skskoari@gmail.com>
2014-09-05 09:33:26 +0900

    * Add new recipe `user::encrypted`    * Separate user::default recipe

--- site-cookbooks/user/recipes/default.rb
+++ site-cookbooks/user/recipes/default.rb
@@ -9,14 +9,6 @@
 
 include_recipe 'group'
 
-data = Chef::EncryptedDataBagItem.load('encrypted_user', 'dev')
-
-user data['id'] do
-  action   :create
-  group    data['group']
-  password `openssl passwd -1 #{data['password']}`.chomp
-end
-
 users = data_bag('user')
 users.each do |user_id|
   u = data_bag_item('user', user_id)
